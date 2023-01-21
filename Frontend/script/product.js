f9f62c9efeb6fb308eaa3e68633f3ff77b2c4164
Soumya Adhya <soumyaadhya007@gmail.com>
2023-01-21 08:03:39 +0530

    day-4

--- Frontend/script/product.js
+++ Frontend/script/product.js
@@ -1,15 +1,5 @@
-const dropdownMenus = document.querySelectorAll('nav ul li');
 
-dropdownMenus.forEach(menu => {
-  menu.addEventListener('click', () => {
-    menu.querySelector('ul').classList.toggle('show');
-  });
-});
-
-// document.addEventListener("click", closeAllSelect);
-function scrollright(){
-  document.getElementById("section3").scrollLeft += 400;
-}
-function scrollleft(){
-  document.getElementById("section3").scrollLeft -= 400;
+function clickimg(smallImg){
+let fullImg=document.getElementById("imagebox");
+fullImg.src=smallImg.src
 }
